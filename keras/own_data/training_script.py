06bc3e4a028cf8528539ba7db23e1c48d3720d64
Gregor Nageler <nageler@mbits.info>
2021-10-20 16:31:59 +0200

    GD-63 Implement Callbacks in training script, not tested

--- keras/own_data/training_script.py
+++ keras/own_data/training_script.py
@@ -1,21 +1,26 @@
+from calendar import c
 from email.policy import default
 import imp
 from pydoc import cli
 import keras
 from matplotlib.pyplot import axis
+from sklearn.utils import shuffle
 from ModelsGenesis.keras.unet3d import *
 from keras.layers import GlobalAveragePooling3D, Dense
 from keras.callbacks import LambdaCallback, TensorBoard, ReduceLROnPlateau
 from keras.utils import to_categorical
 from preprocessing.utils import split_by_cutoff
 import os
+import shutil
 import tensorflow as tf
 from optparse import OptionParser
 import importlib
 import pickle
 import pandas as pd
 
-
+##################
+# OPTION PARSING v
+##################
 # specify config from CLI
 # leave this script as is
 parser = OptionParser()
@@ -30,7 +35,13 @@ conf.display()
 outfile = open(os.path.join(conf.model_path, 'config'), 'wb')
 pickle.dump(conf, outfile)
 outfile.close()
+##################
+# OPTION PARSING ^
+##################
 
+##################
+# GPU SETUP v
+##################
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
@@ -40,6 +51,9 @@ if gpus:
         print(len(gpus), 'Physical GPUS,', len(logical_gpus), 'Locial GPUs')
     except RuntimeError as e:
         print(e)
+##################
+# GPU SETUP ^
+##################
 
 ##################
 # DATA LOADING v
@@ -72,16 +86,9 @@ validation_data = (x_val, y_val)
 # DATA LOADING ^
 ##################
 
-
-########################
-# LOGGING
-########################
-
-########################
-# CALLBACKS
-########################
-
-
+##################
+# MODEL SETUP v
+##################
 models_genesis = unet_model_3d(input_shape=(conf.input_rows, conf.input_cols, conf.input_deps), batch_normalization=True)
 if conf.weights is not None:
     print('Load pre-trained Models Genesis weights from {}'.format(conf.weights))
@@ -93,5 +100,71 @@ x = Dense(1024, activation='relu')(x)
 output = Dense(conf.nb_class, activation=conf.activate)(x)
 model = keras.models.Model(inputs=models_genesis.input, outputs=output)
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-model.fit(X,y, batch_size=conf.batch_size, epochs=conf.nb_epochs, shuffle=True, validation_data=validation_data)
+model.compile(optimizer=conf.optimizer, loss=conf.loss, metrics=conf.metrics)
+##################
+# MODEL SETUP ^
+##################
+
+########################
+# LOGGING & CALLBACKS v
+########################
+if os.path.exists(os.path.join(conf.model_path, conf.exp_name+'.txt')):
+    os.remove(os.path.join(conf.model_path, conf.exp_name+'.txt'))
+with open(os.path.join(conf.model_path, conf.exp_name+'.txt'), 'w') as fh:
+    model.summary(positions=[.3, .55, .67, 1.], print_fn=lambda x: fh.write(x + '\n'))
+
+shutil.rmtree(os.path.join(conf.logs_path, conf.exp_name), ignore_errors=True)
+if not os.path.exists(os.path.join(conf.logs_path, conf.exp_name)):
+    os.makedirs(os.path.join(conf.logs_path, conf.exp_name))
+
+tbCallBack = TensorBoard(
+    log_dir=os.path.join(conf.logs_path, conf.exp_name),
+    histogram_freq=0,
+    write_graph=True,
+    write_images=True,
+)
+tbCallBack.set_model(model)
+
+early_stopping = keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=conf.patience,
+    verbos=0,
+    mode='min',
+    )
+
+check_point = keras.callbacks.ModelCheckpoint(
+    os.path.join(conf.model_path, conf.exp_name+'.h5'),
+    monitor='val_loss',
+    verbose=1,
+    save_best_only=True,
+    mode='min'
+)
+
+lrate_scheduler = ReduceLROnPlateau(
+    monitor='val_loss',
+    factor=0.5,
+    patience=10,
+    min_delta=0.0001,
+    min_lr=1e-6,
+    verbose=1
+)
+callbacks = [check_point, early_stopping, tbCallBack, lrate_scheduler]
+########################
+# LOGGING & CALLBACKS ^
+########################
+
+while conf.batch_size > 1:
+    try:
+        model.fit(
+            X,y,
+            batch_size=conf.batch_size,
+            epochs=conf.nb_epochs,
+            shuffle=True,
+            validation_data=validation_data,
+            callbacks=callbacks,
+            verbose=conf.verbose
+            )
+        break
+    except tf.errors.ResourceExhaustedError as e:
+        conf.batch_size = int(conf.batch_size - 2)
+        print('\n> Batch size = {}'.format(conf.batch_size))
